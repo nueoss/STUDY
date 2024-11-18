d129d7a5e806f6f89f044d91d04e94c4771ae36e
nueoss <0b506h@gmail.com>
2024-11-18 21:43:34 +0900

    스택과 큐 2

--- TEST/src/part1/stackQueue2.java
+++ TEST/src/part1/stackQueue2.java
@@ -1,10 +1,31 @@
 package part1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
 public class stackQueue2 {
+	public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		/* 백준 17298번 오큰수 구하기 */
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(br.readLine()); // 크기
+		
+		StringTokenizer line = new StringTokenizer(br.readLine());
+		int [] arr = new int[size]; // 입력받은 수 배열
+		
+		for(int i = 0; i < size; i++) {
+			arr[i] = Integer.parseInt(line.nextToken());
+		}
+		
+		Stack<Integer> stack = new Stack<Integer>();
+		
+		for(int i = 0; i < size; i++) {
+			
+		}
+		
 	}
-
 }
