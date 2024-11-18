ac1921e310e08d969d3c69082e3c580b72ff3e50
배소은 <0b506h@gamil.com>
2024-11-18 14:24:32 +0900

    슬라이딩 윈도우2

--- TEST/src/part1/slidingWindow2.java
+++ TEST/src/part1/slidingWindow2.java
@@ -28,10 +28,25 @@ public class slidingWindow2 {
 		
 		Deque<Node> nums = new LinkedList<>(); // 숫자 배열 데크 형식
 		
-		for(int i = 0; i < p; i++) {
+		for(int i = 0; i < s; i++) {
+			int now = Integer.parseInt(line2.nextToken());
 			
+			while(!nums.isEmpty() && nums.getLast().value > now) { // 새로운 값보다 이전 값이 크다면 제거
+				nums.removeLast();
+			}
+			
+			nums.addLast(new Node(now, i));
+			
+			// 범위에서 벗어난 인덱스 제거
+			if(nums.getFirst().index <= i - p) {
+				nums.removeFirst();
+			}
+			
+			bw.write(nums.getFirst().value + " ");
 		}
 		
+		bw.flush(); // 출력
+		bw.close();
 	}
 
 	// Node 형식 정하기
