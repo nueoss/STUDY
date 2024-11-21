0ea4c31e2d2127a2b8910d5d7086e7e22f5b7af8
nueoss <0b506h@gmail.com>
2024-11-21 16:10:50 +0900

    버블정렬 1

--- TEST/src/part1/bubbleArray1.java
+++ TEST/src/part1/bubbleArray1.java
@@ -1,9 +1,43 @@
 package part1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
 public class bubbleArray1 {
+	public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) {
-
+		/* 백준 2750번 수 정렬하기1 */
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int count = Integer.parseInt(br.readLine());
+		
+		int[] nums = new int[count];
+		
+		for(int i = 0; i < count; i++) {
+			nums[i] = Integer.parseInt(br.readLine());
+		
+		}
+		
+		// 방법 1 sort() 사용
+		/* Arrays.sort(nums); */
+		
+		// 방법 2 버블 정렬
+		for(int i = 0; i < count - 1; i++) {
+			for(int j = 0; j < count - 1; j++) {
+				if(nums[j] > nums[j+1]) {
+					int temp = nums[j];
+					nums[j] = nums[j+1];
+					nums[j+1] = temp;
+				}
+			}
+		}
+		
+		
+		for(int i = 0; i < count; i++) {
+			System.out.println(nums[i]);
+		}
+				
 	}
-
 }
