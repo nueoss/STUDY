d73d7e41a0f09630741750cf7b2fd5921612e4ee
nueoss <0b506h@gmail.com>
2024-11-26 20:44:53 +0900

    삽입정렬

---
+++ TEST/src/part1/insertionSort1.java
@@ -0,0 +1,18 @@
+package part1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class insertionSort1 {
+	public static void main(String[] args) throws IOException {
+
+		/* 백준 11399번 ATM 인출 시간 계산하기 */
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int size = Integer.parseInt(br.readLine());
+		
+		
+		
+	}
+}
--- TEST/src/part1/insertionSort2.java
+++
@@ -1,9 +0,0 @@
-package part1;
-
-public class insertionSort2 {
-	public static void main(String[] args) {
-
-		
-		
-	}
-}
