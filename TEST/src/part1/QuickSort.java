cd0e48f9c2b035536bb52e0e3833162ab5f866db
nueoss <0b506h@gmail.com>
2024-11-27 18:06:33 +0900

    퀵정렬

--- TEST/src/part1/QuickSort.java
+++ TEST/src/part1/QuickSort.java
@@ -1,10 +1,31 @@
 package part1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
 public class QuickSort {
+	public static void main(String[] args) throws IOException {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+		/* 백준 11004번 K번째 수 구하기 */
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer line1 = new StringTokenizer(br.readLine());
+		
+		int size = Integer.parseInt(line1.nextToken()); // 크기
+		int k = Integer.parseInt(line1.nextToken()); // 크기
+		
+		int [] nums = new int[size];
+		
+		StringTokenizer line2 = new StringTokenizer(br.readLine());
+		for(int i = 0; i < size; i++) {
+			nums[i] = Integer.parseInt(line2.nextToken()); // 크기
+		}
+		
+		Arrays.sort(nums);
+		
+		System.out.println(nums[k]);
+ 	}
 }
