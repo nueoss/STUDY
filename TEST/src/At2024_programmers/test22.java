0828c0f1cffc9259ebebb932571b5a2eda9d1058
nueoss <0b506h@gmail.com>
2026-03-04 22:59:01 +0900

    깃단장_파일정리

---
+++ TEST/src/At2024_programmers/test22.java
@@ -0,0 +1,22 @@
+package At2024_programmers;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class test22 {
+	public static void main(String[] args) {
+
+		String [] phone_book = new String[] {"12", "123", "1245", "88"};
+		boolean result = true;
+		
+		Arrays.sort(phone_book);
+		
+		for (int i = 0; i < phone_book.length - 1; i++) {
+			if (phone_book[i + 1].startsWith(phone_book[i])) result = false;
+        }
+		
+		Map<String, Integer> map = new HashMap<String, Integer>();
+		System.out.println(result);
+	}
+}
